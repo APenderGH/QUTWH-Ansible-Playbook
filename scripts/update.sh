1ac341c2300d053fdc24c6c882d09fb327619192
Samuel <ssamuelpolichronis@gmail.com>
2023-01-14 15:01:03 +1000

    update existing files

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-apt-get update -y
 apt install python3-apt-dbg python-apt-doc python-apt-common python3-apt python3-apt-dbg -y
-#apt-get clean && apt-get update && apt-get dist-upgrade
+apt-get update -y  && apt-get upgrade
