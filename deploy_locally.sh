d344e04fa0cfbdcb0081796f912922c6f08b3090
Samuel <ssamuelpolichronis@gmail.com>
2023-01-15 09:20:30 +1000

    cleaning up

--- deploy_locally.sh
+++ deploy_locally.sh
@@ -11,35 +11,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 
-echo 'This might take over 15 minutes, so curl https://$( cat Starbucks.menu | grep coffee )'
-
-
-sshd='/etc/ssh/sshd_config'
-string1=$(cat $sshd | grep PermitRootLogin )
-string2=$(cat hosts.ini | grep ansible_user=root)
-
-# If you are deplying it with the root user
-if [[ $string2 == *"root"* ]]; then
-	echo "Checking "$sshd ".. "
-	if [[ $string1 == *"yes"* ]]; then
-  		echo "SSH configuration is all set."
-	else
-  		echo "PermitRootLogin is disabled"
-  		echo "Backing up "$sshd ".."
-		cp $sshd $sshd.bk
-  		echo "Modifying "$sshd ".."
-  		sed 's/PermitRootLogin/PermitRootLogin yes#/' $sshd.bk > $sshd
-		systemctl restart ssh
-	fi
-fi
-
-# Fix the source repositories list 
-echo "
-deb http://http.kali.org/kali kali-rolling main non-free contrib
-deb http://kali.cs.nctu.edu.tw/kali kali-rolling main contrib non-free
-deb-src http://http.kali.org/kali kali-rolling main non-free contrib
-" >> /etc/apt/sources.list
-
+echo 'This might take a while, be patient'
 
 # Deploy
 echo 'Updating..'
@@ -47,20 +19,5 @@ bash scripts/update.sh
 # Install ansible
 echo 'Installing Ansible..'
 bash scripts/install-ansible.sh
-# Start SSH
-echo 'Starting SSH..'
-systemctl start ssh
 # Start deploying
 ansible-playbook deploy_kali.yml -i hosts.ini -e 'ansible_python_interpreter=/usr/bin/python3'
-
-
-# If you are deplying it with the root user
-if [[ $string2 == *"root"* ]]; then
-	if [[ $string1 == *"yes"* ]]; then
-  		echo "All set. Hack the planet!"
-	else
-		echo "Recovering "$sshd ".."
-		# Recover the old sshd_config file
-		mv $sshd.bk $sshd
-	fi
-fi
